479b2c36c5bd757a93630bd5c53a5b3e8ba352a8
alexandermaxwellogle <maxogle@gmail.com>
2022-02-23 23:04:12 -0500

    commit tree 58d7a46f990a24d454fba5855376600ee296aa77

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.body.remove(main);
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "Max is the champion";
+document.body.append(newHeader);
+
